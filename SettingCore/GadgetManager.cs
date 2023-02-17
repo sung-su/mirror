c1ba137087d9da33d9b7b60689795be779c2b2f4
Marcin Romniuk <m.romaniuk@samsung.com>
2023-02-17 23:48:02 +0100

    add GadgetManager

---
+++ SettingCore/GadgetManager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Tizen.NUI;
+
+namespace SettingCore
+{
+    public class GadgetManager
+    {
+        private const string SettingGadgetPackagePrefix = "org.tizen.setting";
+        private const string SettingMenuMetadataPrefix = "setting/menu/";
+
+        // dict key is metadata key from tizen-manifest.xml file (e.g. setting/menu/wifi)
+        private static Dictionary<string, SettingGadgetInfo> gadgets;
+
+        static GadgetManager()
+        {
+            gadgets = getSettingGadgets();
+        }
+
+        private static Dictionary<string, SettingGadgetInfo> getSettingGadgets()
+        {
+            Dictionary<string, SettingGadgetInfo> dict = new Dictionary<string, SettingGadgetInfo>();
+
+            var allGadgetPackages = NUIGadgetManager.GetGadgetInfos();
+            var settingGadgetPackages = allGadgetPackages.Where(pkg => pkg.PackageId.StartsWith(SettingGadgetPackagePrefix));
+
+            Logger.Debug($"all gadget packages: {allGadgetPackages.Count()}, setting gadget packages: {settingGadgetPackages.Count()}");
+            foreach (var pkg in settingGadgetPackages)
+            {
+                Logger.Debug($"setting gadget package (pkgId: {pkg.PackageId}, resType: {pkg.ResourceType})");
+            }
+            foreach (var pkg in allGadgetPackages.Where(p => !settingGadgetPackages.Contains(p)))
+            {
+                Logger.Debug($"other gadget package (pkgId: {pkg.PackageId}, resType: {pkg.ResourceType})");
+            }
+
+            foreach (var pkg in settingGadgetPackages)
+            {
+                var metadatas = pkg.Metadata.Where(pair => pair.Key.StartsWith(SettingMenuMetadataPrefix));
+                foreach (var metadata in metadatas)
+                {
+                    if (!int.TryParse(metadata.Value, out int orderId))
+                    {
+                        Logger.Debug($"metadata value (orderId) is not integer (pkgId: {pkg.PackageId}, resType: {pkg.ResourceType}, key:{metadata.Key}, value:{metadata.Value})");
+                        continue;
+                    }
+
+                    var parts = metadata.Key.Split('/');
+                    var partsCapitalized = parts.Select(s => s[..1].ToUpperInvariant() + s[1..]);
+                    string className = string.Join('.', partsCapitalized) + "Gadget";
+
+                    bool isMainMenu = parts.Length == 3; // depends on SettingMenuMetadataPrefix.Length
+
+                    var settingGadgetInfo = new SettingGadgetInfo(pkg, orderId, className, isMainMenu);
+                    Logger.Debug($"found gadget ({settingGadgetInfo})");
+
+                    dict.Add(metadata.Key, settingGadgetInfo);
+                }
+            }
+
+            return dict;
+        }
+
+        public static IEnumerable<SettingGadgetInfo> GetAll()
+        {
+            return gadgets
+                .Select(pair => pair.Value);
+        }
+
+        public static IEnumerable<SettingGadgetInfo> GetMain()
+        {
+            return gadgets
+                .Select(pair => pair.Value)
+                .Where(info => info.IsMainMenu)
+                .OrderBy(info => info.OrderId);
+        }
+
+        public static Dictionary<string, int> GetDefaultCustomization()
+        {
+            var customization = gadgets
+                .Select(pair => new KeyValuePair<string, int>(pair.Key, pair.Value.OrderId));
+
+            return new Dictionary<string, int>(customization);
+        }
+    }
+}
---
+++ SettingCore/SettingGadgetInfo.cs
@@ -0,0 +1,22 @@
+﻿using Tizen.NUI;
+
+namespace SettingCore
+{
+    public class SettingGadgetInfo
+    {
+        public NUIGadgetInfo Pkg { get; private set; }
+        public int OrderId { get; private set; }
+        public string ClassName { get; private set; }
+        public bool IsMainMenu { get; private set; }
+
+        public SettingGadgetInfo(NUIGadgetInfo pkg, int orderId, string className, bool isMainMenu)
+        {
+            Pkg = pkg;
+            OrderId = orderId;
+            ClassName = className;
+            IsMainMenu = isMainMenu;
+        }
+
+        public override string ToString() => $"pkgId: {Pkg.PackageId}, class: {ClassName}, orderId: {OrderId}, main: {IsMainMenu}";
+    }
+}
