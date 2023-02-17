0d1fb980e55ede2784e6536c67afd04f9c581fd5
Marcin Romniuk <m.romaniuk@samsung.com>
2023-02-17 23:53:06 +0100

    add main menu info

---
+++ SettingCore/ColorExtension.cs
@@ -0,0 +1,21 @@
+﻿using Tizen.NUI;
+
+namespace SettingCore
+{
+    public static class ColorExtension
+    {
+        public static string ToHex(this Color color)
+        {
+            static string toHex(float f)
+            {
+                int i = (int)(f * 255);
+                return string.Format($"{i:X2}");
+            }
+            string r = toHex(color.R);
+            string g = toHex(color.G);
+            string b = toHex(color.B);
+            string a = color.A == 1 ? "" : toHex(color.A);
+            return $"#{r}{g}{b}{a}";
+        }
+    }
+}
---
+++ SettingCore/MainMenuInfo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using Tizen.NUI;
+
+namespace SettingCore
+{
+    public class MainMenuInfo
+    {
+        public string IconPath { get; private set; }
+        public Color IconColor { get; private set; }
+        public string Title { get; private set; }
+        public Action Action { get; private set; }
+
+        public static IEnumerable<MainMenuInfo> GetReal()
+        {
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
+            var list = new List<MainMenuInfo>();
+            var infos = GadgetManager.GetMain();
+            foreach (var info in infos)
+            {
+                NUIGadget gadget;
+                try
+                {
+                    gadget = NUIGadgetManager.Add(info.Pkg.ResourceType, info.ClassName);
+                }
+                catch
+                {
+                    Logger.Warn($"real main menu - could not create gadget with ClassName {info.ClassName} and ResourceType {info.Pkg.ResourceType}");
+                    continue;
+                }
+
+                if (gadget is MainMenuGadget mainMenuGadget)
+                {
+                    string iconPath = mainMenuGadget.ProvideIconPath();
+                    Color iconColor = mainMenuGadget.ProvideIconColor();
+                    string title = mainMenuGadget.ProvideTitle();
+                    Action action = () =>
+                    {
+                        Logger.Debug($"navigating to {info.ClassName}, title: {title}");
+                        GadgetNavigation.NavigateTo(info.ClassName);
+                    };
+
+                    // FIXME: currently menus return relative icon path, so here we make it absolute
+                    iconPath = System.IO.Path.Combine(Tizen.Applications.Application.Current.DirectoryInfo.Resource, iconPath);
+
+                    Logger.Debug($"real main menu ({info.ClassName}) title: {title}, icon color: {iconColor.ToHex()}, icon path: {iconPath}");
+                    list.Add(new MainMenuInfo
+                    {
+                        IconPath = iconPath,
+                        IconColor = iconColor,
+                        Title = title,
+                        Action = action,
+                    });
+                }
+                else
+                {
+                    Logger.Warn($"real main menu - {info.ClassName} is not MainMenuGadget");
+                }
+
+                NUIGadgetManager.Remove(gadget);
+            }
+
+            stopwatch.Stop();
+            Logger.Debug($"real main menu gadgets scanned in {stopwatch.Elapsed}");
+
+            return list;
+        }
+    }
+}
