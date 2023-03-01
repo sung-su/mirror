f01bcd183a636d0ceb93027e4281f103049731e0
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2023-03-01 17:06:06 +0100

    add theme gadget

---
+++ SettingMainGadget/Display/DisplayThemeGadget.cs
@@ -0,0 +1,61 @@
+﻿using SettingAppTextResopurces.TextResources;
+using SettingCore;
+using SettingMainGadget.Display;
+using System.Collections.ObjectModel;
+using System.Linq;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace Setting.Menu.Display
+{
+    public class DisplaythemeGadget : SettingCore.MenuGadget
+    {
+        public override string ProvideTitle() => Resources.IDS_ST_BODY_THEME;
+
+        protected override View OnCreate()
+        {
+            var content = new View()
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                Layout = new LinearLayout()
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                },
+            };
+
+            var picker = new Picker()
+            {
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+            };
+
+            ReadOnlyCollection<string> rc = new ReadOnlyCollection<string>(DisplayThemeManager.ThemeList.Select(a => a.GetName()).ToList());
+            picker.DisplayedValues = rc;
+            picker.MinValue = 0;
+            picker.MaxValue = DisplayThemeManager.ThemeList.Count - 1;
+            picker.CurrentValue = DisplayThemeManager.GetThemeIndex();
+
+            Logger.Debug($"ThemePicker CurrentValue: {picker.CurrentValue}");
+
+            var button = new Button()
+            {
+                Text = Resources.IDS_ST_BUTTON_OK
+            };
+
+            button.Clicked += (bo, be) =>
+            {
+                DisplayThemeManager.SetTheme(DisplayThemeManager.ThemeList[picker.CurrentValue].GetId());
+
+                NavigateBack();
+            };
+
+            content.Add(picker);
+            content.Add(button);
+
+            return content;
+        }
+    }
+}
---
+++ SettingMainGadget/Display/DisplayThemeManager.cs
@@ -0,0 +1,81 @@
+﻿using SettingAppTextResopurces.TextResources;
+using SettingCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tizen.NUI;
+
+namespace SettingMainGadget.Display
+{
+    public class DisplayThemeManager
+    {
+        public class ThemeInfo
+        {
+            private string Name;
+            private string Id;
+
+            public ThemeInfo(string name, string id)
+            {
+                Name = name;
+                Id = id;
+            }
+
+            public string GetName()
+            {
+                return Name;
+            }
+
+            public string GetId()
+            {
+                return Id;
+            }
+        };
+
+        public static readonly List<ThemeInfo> ThemeList = new List<ThemeInfo>
+        {
+            new ThemeInfo("Light theme", "org.tizen.default-light-theme"),
+            new ThemeInfo("Dark theme", "org.tizen.default-dark-theme"),
+        }; // TODO : add name of the theme to the Resources so it can be translated when changing the language
+
+        public static void SetTheme(string Id)
+        {
+            try
+            {
+                ThemeManager.ApplyPlatformTheme(Id);
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"({Id}): {ex.Message}");
+            }
+        }
+
+        public static int GetThemeIndex()
+        {
+            string themeId = ThemeManager.PlatformThemeId;
+
+            if (string.IsNullOrEmpty(themeId))
+            {
+                Logger.Warn("Theme : Not Available");
+                return -1;
+            }
+
+            Logger.Debug($"Theme : {themeId}");
+
+            var theme = ThemeList.Where(x => x.GetId().Equals(themeId)).First();
+
+            if(theme != null)
+            {
+                return ThemeList.IndexOf(theme); 
+            }
+
+            return -1;
+        }
+
+        public static string GetThemeName()
+        {
+            int index = GetThemeIndex();
+
+            return index >= 0 ? ThemeList[index].GetName() : Resources.IDS_ST_HEADER_UNAVAILABLE;
+        }
+    }
+}
