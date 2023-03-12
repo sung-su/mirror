564eadb8a7d5322c3acf2581107e468f13025c8d
Marcin Romaniuk <m.romaniuk@samsung.com>
2023-03-12 22:29:31 +0100

    add script build nuget package

---
+++ build-nuget-setting-core.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/bash
+PROJECT=SettingCore/SettingCore.csproj
+CONFIGURATION=Debug
+OUTDIR=artifacts
+
+VERSION_SUFFIX=$((10000+$(git rev-list --count HEAD)))
+VERSION_BASE=1.0.0
+VERSION=$VERSION_BASE-$VERSION_SUFFIX
+
+dotnet clean $PROJECT
+rm -rf SettingCore/bin SettingCore/obj artifacts
+
+dotnet restore --force $PROJECT
+
+dotnet build --no-restore -c $CONFIGURATION $PROJECT
+
+dotnet pack --no-restore --no-build --nologo \
+    $PROJECT \
+    -o $OUTDIR \
+    -p:Version=$VERSION
+
