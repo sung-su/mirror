74c462598fe605eb605abdd840dcbc17f88e7d9f
Marcin Romaniuk <m.romaniuk@samsung.com>
2023-03-22 22:20:55 +0100

    update back button with theme colors

--- SettingCore/Views/BackButton.cs
+++ SettingCore/Views/BackButton.cs
@@ -1,97 +1,53 @@
-﻿using System;
-using Tizen.NUI;
+﻿using Tizen.NUI;
 using Tizen.NUI.BaseComponents;
-using Tizen.NUI.Components;
 
 namespace SettingCore.Views
 {
-    public class BackButton : View
+    public class BackButton : BaseComponent
     {
-        internal class Colors
-        {
-            internal class Background
-            {
-                internal static readonly Color Normal = Color.White;
-                internal static readonly Color Selected = new Color("#FF6200").WithAlpha(0.3f);
-            }
-            internal class Icon
-            {
-                internal static readonly Color Normal = Color.Black;
-                internal static readonly Color Selected = new Color("#FF6200");
-            }
-        }
+        private readonly ThemeColor BackgroundColors = new ThemeColor(Color.Transparent, Color.Transparent, new Color("#FF6200").WithAlpha(0.1f), Color.White.WithAlpha(0.16f));
+        private readonly ThemeColor IconColors = new ThemeColor(new Color("#17234D"), new Color("#FDFDFD"), new Color("#FF6200"), new Color("#FF8A00"));
 
         private readonly ImageView icon;
         private readonly ImageVisual iconVisual;
 
-        public event EventHandler<ClickedEventArgs> Clicked;
-
-        public BackButton()
+        public BackButton() : base()
         {
-            BackgroundColor = Colors.Background.Normal;
+            BackgroundColor = BackgroundColors.Normal;
             CornerRadius = 8f.SpToPx();
             Size = new Size(40, 40).SpToPx();
 
-            LeaveRequired = true;
-
             iconVisual = new ImageVisual
             {
-                MixColor = Colors.Icon.Normal,
+                MixColor = IconColors.Normal,
                 URL = System.IO.Path.Combine(Tizen.Applications.Application.Current.DirectoryInfo.Resource, "back-button-icon.svg"),
                 FittingMode = FittingModeType.ScaleToFill,
             };
             icon = new ImageView
             {
                 Image = iconVisual.OutputVisualMap,
-                Size = new Size(40, 40).SpToPx(),
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
             };
             Add(icon);
-
-            TouchEvent += OnTouchEvent;
         }
 
-        private void ChangeSelect(bool select)
+        public override void OnChangeSelected(bool selected)
         {
-            if (select)
+            if (selected)
             {
-                BackgroundColor = Colors.Background.Selected;
-
-                iconVisual.MixColor = Colors.Icon.Selected;
+                iconVisual.MixColor = IconColors.Selected;
                 icon.Image = iconVisual.OutputVisualMap;
+
+                BackgroundColor = BackgroundColors.Selected;
             }
             else
             {
-                BackgroundColor = Colors.Background.Normal;
-
-                iconVisual.MixColor = Colors.Icon.Normal;
+                iconVisual.MixColor = IconColors.Normal;
                 icon.Image = iconVisual.OutputVisualMap;
+
+                BackgroundColor = BackgroundColors.Normal;
             }
-        }
-
-        private bool touchStarted = false;
-        private bool OnTouchEvent(object source, TouchEventArgs e)
-        {
-            var state = e.Touch.GetState(0);
-
-            if (state == PointStateType.Down)
-            {
-                touchStarted = true;
-            }
-            else if (state == PointStateType.Finished && touchStarted)
-            {
-                touchStarted = false;
-
-                var handler = Clicked;
-                handler?.Invoke(this, new ClickedEventArgs());
-            }
-            else
-            {
-                touchStarted = false;
-            }
-
-            ChangeSelect(state == PointStateType.Down);
-
-            return true;
         }
     }
 }
