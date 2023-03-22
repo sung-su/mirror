1d0d4c4213c48a9cd435c5e5fd40872dee6668cb
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2023-03-22 16:46:44 +0100

    add base component

---
+++ SettingCore/Views/BaseComponent.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace SettingCore.Views
+{
+    public class BaseComponent : Control
+    {
+        public BaseComponent()
+        {
+            WidthSpecification = LayoutParamPolicies.MatchParent;
+            HeightSpecification = LayoutParamPolicies.WrapContent;
+            LeaveRequired = true;
+            AccessibilityHighlightable = true;
+
+            TouchEvent += OnTouchEvent;
+        }
+
+        public event EventHandler<ClickedEventArgs> Clicked;
+
+        private bool touchStarted = false;
+
+        public virtual void OnChangeSelected(bool selected) { }
+
+        public override bool OnKey(Key key)
+        {
+            if (!IsEnabled || null == key)
+            {
+                return false;
+            }
+
+            if (key.State == Key.StateType.Up)
+            {
+                if (key.KeyPressedName == "Return")
+                {
+                    var handler = Clicked;
+                    handler?.Invoke(this, new ClickedEventArgs());
+                }
+            }
+            return base.OnKey(key);
+        }
+
+        private bool OnTouchEvent(object source, TouchEventArgs e)
+        {
+            if (Clicked is null)
+            {
+                return false;
+            }
+
+            var state = e.Touch.GetState(0);
+
+            if (state == PointStateType.Down)
+            {
+                touchStarted = true;
+            }
+            else if (state == PointStateType.Finished && touchStarted)
+            {
+                touchStarted = false;
+
+                var handler = Clicked;
+                handler?.Invoke(this, new ClickedEventArgs());
+            }
+            else
+            {
+                touchStarted = false;
+            }
+
+            OnChangeSelected(state == PointStateType.Down);
+
+            return true;
+        }
+    }
+}
