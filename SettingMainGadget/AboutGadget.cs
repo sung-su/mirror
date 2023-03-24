15ebd94857cfc57093c6f787b02cfc086a6a1122
Marcin Romaniuk <m.romaniuk@samsung.com>
2023-03-24 14:46:38 +0100

    update About menu with UX guide

--- SettingMainGadget/AboutGadget.cs
+++ SettingMainGadget/AboutGadget.cs
@@ -1,9 +1,9 @@
 ﻿using SettingAppTextResopurces.TextResources;
 using SettingCore;
 using SettingCore.Customization;
+using SettingCore.Views;
 using SettingMain;
 using SettingMainGadget;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Tizen;
@@ -17,7 +17,7 @@ namespace Setting.Menu
     public class AboutGadget : SettingCore.MainMenuGadget
     {
         private const int MAX_DEVICE_NAME_LEN = 32;
-        private DefaultLinearItem mDevicenameItem;
+        private TextListItem renameDevice;
         private string VconfDeviceName = "db/setting/device_name";
         private string SystemModelName = "tizen.org/system/model_name";
 
@@ -60,8 +60,8 @@ namespace Setting.Menu
 
         private void SystemSettings_DeviceNameChanged(object sender, DeviceNameChangedEventArgs e)
         {
-            Logger.Warn($"Device name changed: {e.Value}");
-            mDevicenameItem.SubText = e.Value;
+            Logger.Verbose($"Device name changed: {e.Value}");
+            renameDevice.Secondary = e.Value;
         }
 
 
@@ -69,140 +69,67 @@ namespace Setting.Menu
         {
             sections.RemoveAllSectionsFromView(content);
 
-            DefaultLinearItem item = null;
-
-            item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_HEADER_MANAGE_CERTIFICATES_ABB);
-            sections.Add(MainMenuProvider.About_ManageCertificates, item);
-
-            item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_BODY_OPEN_SOURCE_LICENCES);
-            sections.Add(MainMenuProvider.About_OpenSourceLicenses, item);
-            if (item != null)
+            var manageCertificates = TextListItem.CreatePrimaryTextItem(Resources.IDS_ST_HEADER_MANAGE_CERTIFICATES_ABB);
+            manageCertificates.Clicked += (s, e) =>
             {
-                item.Clicked += (o, e) =>
-                {
-                    NavigateTo(MainMenuProvider.About_OpenSourceLicenses);
-                };
-            }
-            sections.Add(MainMenuProvider.About_DeviceInfo, SettingItemCreator.CreateItemStatic(Resources.IDS_ST_BODY_DEVICE_INFO));
+                NavigateTo(MainMenuProvider.About_ManageCertificates);
+            };
+            sections.Add(MainMenuProvider.About_ManageCertificates, manageCertificates);
+
+            var openSourceLicenses = TextListItem.CreatePrimaryTextItem(Resources.IDS_ST_BODY_OPEN_SOURCE_LICENCES);
+            openSourceLicenses.Clicked += (s, e) =>
+            {
+                NavigateTo(MainMenuProvider.About_OpenSourceLicenses);
+            };
+            sections.Add(MainMenuProvider.About_OpenSourceLicenses, openSourceLicenses);
+
+            // TODO: replace with header text (gray)
+            var deviceInfo = TextListItem.CreatePrimaryTextItem(Resources.IDS_ST_BODY_DEVICE_INFO);
+            sections.Add(MainMenuProvider.About_DeviceInfo, deviceInfo);
 
             if (Vconf.TryGetString(VconfDeviceName, out string name))
             {
                 Logger.Warn($"Could not get vconf value: {VconfDeviceName}");
             }
-            item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_BODY_NAME, name);
-            mDevicenameItem = item;
-            if (item != null)
+
+            renameDevice = TextListItem.CreatePrimaryTextItemWithSecondaryText(Resources.IDS_ST_BODY_NAME, name);
+            renameDevice.Clicked += (s, e) =>
             {
-                item.Clicked += (o, e) =>
-                {
-                    var content = new View()
-                    {
-                        BackgroundColor = new Color("#FAFAFA"),
-                        WidthSpecification = LayoutParamPolicies.MatchParent,
-                        HeightSpecification = LayoutParamPolicies.MatchParent,
-                        Layout = new LinearLayout()
-                        {
-                            HorizontalAlignment = HorizontalAlignment.Center,
-                            LinearOrientation = LinearLayout.Orientation.Vertical,
-                        },
-                    };
+                ShowRenamePopup(name);
+            };
+            sections.Add(MainMenuProvider.About_RenameDevice, renameDevice);
 
-                    var textTitle = SettingItemCreator.CreateItemTitle(Resources.IDS_ST_HEADER_RENAME_DEVICE);
-                    textTitle.Margin = new Extents(24, 0, 0, 0).SpToPx();
-                    content.Add(textTitle);
+            bool result = Tizen.System.Information.TryGetValue<string>("http://tizen.org/system/model_name", out string modelNumberText);
+            var modelNumber = TextListItem.CreatePrimaryTextItemWithSecondaryText(Resources.IDS_ST_BODY_MODEL_NUMBER, result ? modelNumberText : Resources.IDS_ST_HEADER_UNAVAILABLE);
+            sections.Add(MainMenuProvider.About_ModelNumber, modelNumber);
 
-                    var textSubTitle = new TextLabel(Resources.IDS_ST_BODY_DEVICE_NAMES_ARE_DISPLAYED)
-                    {
-                        MultiLine = true,
-                        LineWrapMode = LineWrapMode.Character,
-                        Size = new Size(Window.Instance.WindowSize.Width - 20 * 2, 100),
-                        Margin = new Extents(24, 24, 0, 0).SpToPx(),
-                    };
-                    content.Add(textSubTitle);
+            result = Tizen.System.Information.TryGetValue<string>("http://tizen.org/feature/platform.version", out string platformVersionText);
+            var platformVersion = TextListItem.CreatePrimaryTextItemWithSecondaryText(Resources.IDS_ST_MBODY_TIZEN_VERSION, result ? platformVersionText : Resources.IDS_ST_HEADER_UNAVAILABLE);
+            sections.Add(MainMenuProvider.About_TizenVersion, platformVersion);
 
-                    PropertyMap placeholder = new PropertyMap();
-                    placeholder.Add("color", new PropertyValue(Color.CadetBlue));
-                    placeholder.Add("fontFamily", new PropertyValue("Serif"));
-                    placeholder.Add("pointSize", new PropertyValue(25.0f));
-
-                    var textField = new TextField
-                    {
-                        BackgroundColor = Color.White,
-
-                        Placeholder = placeholder,
-
-                        MaxLength = MAX_DEVICE_NAME_LEN,
-                        EnableCursorBlink = true,
-                        Text = name,
-                    };
-                    content.Add(textField);
-
-                    var button = new Button()
-                    {
-                        Text = Resources.IDS_ST_BUTTON_OK,
-                        Margin = new Extents(0, 0, 0, 32).SpToPx(),
-                    };
-                    button.Clicked += (o, e) =>
-                    {
-                        // Change Device Name
-                        Vconf.SetString("db/setting/device_name", textField.Text);
-                        NUIApplication.GetDefaultWindow().GetDefaultNavigator().Pop();
-                    };
-                    content.Add(button);
-
-                    RoundedDialogPage.ShowDialog(content);
-                };
-            }
-            sections.Add(MainMenuProvider.About_RenameDevice, item);
-
-            string valuestring;
-            bool result = Tizen.System.Information.TryGetValue<string>("http://tizen.org/system/model_name", out valuestring);
-            if (result)
-                item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_BODY_MODEL_NUMBER, valuestring);
-            else
-                item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_BODY_MODEL_NUMBER, Resources.IDS_ST_HEADER_UNAVAILABLE);
-            sections.Add(MainMenuProvider.About_ModelNumber, item);
-
-            result = Tizen.System.Information.TryGetValue<string>("http://tizen.org/feature/platform.version", out valuestring);
-            if (result)
-                item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_MBODY_TIZEN_VERSION, valuestring);
-            else
-                item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_MBODY_TIZEN_VERSION, Resources.IDS_ST_HEADER_UNAVAILABLE);
-            sections.Add(MainMenuProvider.About_TizenVersion, item);
-
-            result = Tizen.System.Information.TryGetValue<string>("http://tizen.org/system/platform.processor", out valuestring);
-            if (result)
-                item = SettingItemCreator.CreateItemWithCheck("CPU", valuestring);
-            else
-                item = SettingItemCreator.CreateItemWithCheck("CPU", Resources.IDS_ST_HEADER_UNAVAILABLE);
-            sections.Add(MainMenuProvider.About_Cpu, item);
+            result = Tizen.System.Information.TryGetValue<string>("http://tizen.org/system/platform.processor", out string platformProcessorText);
+            var cpu = TextListItem.CreatePrimaryTextItemWithSecondaryText("CPU", result ? platformProcessorText : Resources.IDS_ST_HEADER_UNAVAILABLE);
+            sections.Add(MainMenuProvider.About_Cpu, cpu);
 
             var memusage = new Tizen.System.SystemMemoryUsage();
-            float ram_total_gb = memusage.Total / 1000000.0F;
-            item = SettingItemCreator.CreateItemWithCheck("RAM", String.Format("{0:0.0} GB", ram_total_gb));
-            sections.Add(MainMenuProvider.About_Ram, item);
+            float ram_total_gb = memusage.Total / (float)(1024 * 1024);
+            var ram = TextListItem.CreatePrimaryTextItemWithSecondaryText("RAM", string.Format("{0:0.0} GB", ram_total_gb));
+            sections.Add(MainMenuProvider.About_Ram, ram);
 
             bool result1 = Tizen.System.Information.TryGetValue<int>("http://tizen.org/feature/screen.width", out int screenwidth);
             bool result2 = Tizen.System.Information.TryGetValue<int>("http://tizen.org/feature/screen.height", out int screenheight);
 
-            if (result1 && result2)
-                item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_BODY_RESOLUTION, String.Format("{0:d} x {1:d}", screenwidth, screenheight));
-            else
-                item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_BODY_RESOLUTION, Resources.IDS_ST_HEADER_UNAVAILABLE);
-            sections.Add(MainMenuProvider.About_Resolution, item);
+            var resolution = TextListItem.CreatePrimaryTextItemWithSecondaryText(Resources.IDS_ST_BODY_RESOLUTION, result1 && result2 ? $"{screenwidth} x {screenheight}" : Resources.IDS_ST_HEADER_UNAVAILABLE);
+            sections.Add(MainMenuProvider.About_Resolution, resolution);
 
             if (IsEmulBin() == false)
             {
-                item = SettingItemCreator.CreateItemWithCheck(Resources.IDS_ST_BODY_STATUS, Resources.IDS_ST_BODY_SHOW_NETWORK_STATUS_AND_OTHER_INFORMATION);
-                if (item != null)
+                var showOther = TextListItem.CreatePrimaryTextItemWithSecondaryText(Resources.IDS_ST_BODY_STATUS, Resources.IDS_ST_BODY_SHOW_NETWORK_STATUS_AND_OTHER_INFORMATION);
+                showOther.Clicked += (s, e) =>
                 {
-
-                    item.Clicked += (o, e) =>
-                    {
-                        NavigateTo(MainMenuProvider.About_DeviceStatus);
-                    };
-                    sections.Add(MainMenuProvider.About_DeviceStatus, item);
-                }
+                    NavigateTo(MainMenuProvider.About_DeviceStatus);
+                };
+                sections.Add(MainMenuProvider.About_DeviceStatus, showOther);
             }
 
             // add only visible sections to content view in required order
@@ -228,6 +155,66 @@ namespace Setting.Menu
         {
             Information.TryGetValue<string>(SystemModelName, out string model);
             return (model.Equals("Emulator") || model.Equals("EMULATOR"));
+        }
+
+        private void ShowRenamePopup(string name)
+        {
+            var content = new View()
+            {
+                BackgroundColor = new Color("#FAFAFA"),
+                WidthSpecification = LayoutParamPolicies.MatchParent,
+                HeightSpecification = LayoutParamPolicies.MatchParent,
+                Layout = new LinearLayout()
+                {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    LinearOrientation = LinearLayout.Orientation.Vertical,
+                },
+            };
+
+            var textTitle = SettingItemCreator.CreateItemTitle(Resources.IDS_ST_HEADER_RENAME_DEVICE);
+            textTitle.Margin = new Extents(24, 0, 0, 0).SpToPx();
+            content.Add(textTitle);
+
+            var textSubTitle = new TextLabel(Resources.IDS_ST_BODY_DEVICE_NAMES_ARE_DISPLAYED)
+            {
+                MultiLine = true,
+                LineWrapMode = LineWrapMode.Character,
+                Size = new Size(Window.Instance.WindowSize.Width - 20 * 2, 100),
+                Margin = new Extents(24, 24, 0, 0).SpToPx(),
+            };
+            content.Add(textSubTitle);
+
+            PropertyMap placeholder = new PropertyMap();
+            placeholder.Add("color", new PropertyValue(Color.CadetBlue));
+            placeholder.Add("fontFamily", new PropertyValue("Serif"));
+            placeholder.Add("pointSize", new PropertyValue(25.0f));
+
+            var textField = new TextField
+            {
+                BackgroundColor = Color.White,
+
+                Placeholder = placeholder,
+
+                MaxLength = MAX_DEVICE_NAME_LEN,
+                EnableCursorBlink = true,
+                Text = name,
+            };
+            content.Add(textField);
+
+            var button = new Button()
+            {
+                Text = Resources.IDS_ST_BUTTON_OK,
+                Margin = new Extents(0, 0, 0, 32).SpToPx(),
+            };
+            button.Clicked += (o, e) =>
+            {
+                // Change Device Name
+                Vconf.SetString("db/setting/device_name", textField.Text);
+                NUIApplication.GetDefaultWindow().GetDefaultNavigator().Pop();
+            };
+            content.Add(button);
+
+            RoundedDialogPage.ShowDialog(content);
         }
     }
 }
