808c46cd55c3f19f26c79f1ec193fdee4ef634a3
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2023-03-27 09:27:01 +0200

    update sound mode picker -> radiobuttons

--- SettingMainGadget/Sound/SoundmodeGadget.cs
+++ SettingMainGadget/Sound/SoundmodeGadget.cs
@@ -1,6 +1,5 @@
 ﻿using SettingAppTextResopurces.TextResources;
 using SettingMainGadget.Sound;
-using System.Collections.ObjectModel;
 using System.Linq;
 using Tizen.NUI;
 using Tizen.NUI.BaseComponents;
@@ -22,41 +21,39 @@ namespace Setting.Menu.Sound
         {
             base.OnCreate();
 
-            var picker = new Picker()
-            {
-                WidthSpecification = LayoutParamPolicies.MatchParent, // FIXME: required for Picker to draw correctly
-            };
-
-            picker.DisplayedValues = new ReadOnlyCollection<string>(sounds.Select(s => SoundmodeManager.GetSoundmodeName(s)).ToArray());
-            picker.MinValue = 0;
-            picker.MaxValue = sounds.Length - 1;
-            picker.CurrentValue = sounds.ToList().IndexOf(SoundmodeManager.GetSoundmode());
-
-            var button = new Button("Tizen.NUI.Components.Button.Outlined")
-            {
-                Text = Resources.IDS_ST_BUTTON_OK
-            };
-            button.Clicked += (bo, be) =>
-            {
-                var current = sounds[picker.CurrentValue];
-                SoundmodeManager.SetSoundmode(current);
-
-                NavigateBack();
-            };
-
             var content = new View()
             {
                 WidthSpecification = LayoutParamPolicies.MatchParent,
                 HeightSpecification = LayoutParamPolicies.MatchParent,
                 Layout = new LinearLayout()
                 {
-                    HorizontalAlignment = HorizontalAlignment.Center,
-                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Begin,
+                    VerticalAlignment = VerticalAlignment.Top,
                     LinearOrientation = LinearLayout.Orientation.Vertical,
                 },
             };
-            content.Add(picker);
-            content.Add(button);
+
+            RadioButtonGroup radioButtonGroup = new RadioButtonGroup();
+
+            for (int i = 0; i < sounds.Length; i++)
+            {
+                RadioButton radioButton = new RadioButton()
+                {
+                    ThemeChangeSensitive = true,
+                    Text = SoundmodeManager.GetSoundmodeName(sounds[i]),
+                    IsSelected = i.Equals(sounds.ToList().IndexOf(SoundmodeManager.GetSoundmode())),
+                    Margin = new Extents(24, 0, 0, 0).SpToPx(),
+                };
+
+                radioButtonGroup.Add(radioButton);
+                content.Add(radioButton);
+            }
+
+            radioButtonGroup.SelectedChanged += (o, e) =>
+            {
+                var current = sounds[radioButtonGroup.SelectedIndex];
+                SoundmodeManager.SetSoundmode(current);
+            };
 
             return content;
         }
