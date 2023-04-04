4a2790452a2012afd6c5fed8c47c908ddcf8df19
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2023-04-04 13:15:11 +0200

    add base content page with accessibility name

---
+++ SettingCore/Views/BaseContentPage.cs
@@ -0,0 +1,9 @@
+﻿using Tizen.NUI.Components;
+
+namespace SettingCore.Views
+{
+    public class BaseContentPage : ContentPage
+    {
+        protected override string AccessibilityGetName() => AppBar?.Title;
+    }
+}
