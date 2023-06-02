7e2c69b22fea1350cce8610ddf07792590d17d6b
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2023-06-02 10:20:18 +0200

    add certificate manager

---
+++ SettingMainGadget/SettingMainGadget/About/SettingCertificateManager.cs
@@ -0,0 +1,38 @@
+﻿using SettingCore;
+using System;
+using System.Collections.Generic;
+using static Interop;
+using static Interop.CertSvc;
+
+namespace SettingMainGadget.About
+{
+    class SettingCertificateManager
+    {
+        public static certificateMetadata CertificateMetadata { get; set; }
+
+        public static List<certificateMetadata> GetRootCertList()
+        {
+            int result = CertSvc.InstanceNew(out IntPtr instance);
+            if ((CertSvc.ErrorCode)result != CertSvc.ErrorCode.CERTSVC_SUCCESS)
+            {
+                Logger.Warn($"Unable to create cert instance, result: {result}");
+                return new List<certificateMetadata>();
+            }
+
+            result = CertSvc.GetPKCS12CertificateListFromStore(instance, CertSvc.CertStoreType.ALL_STORE, 0, out IntPtr storeCertList, out int length);
+            if ((CertSvc.ErrorCode)result != CertSvc.ErrorCode.CERTSVC_SUCCESS)
+            {
+                Logger.Warn($"Unable to get root certificates, result: {result}");
+                result = CertSvc.FreePKCS12CertificateListFromStore(instance, out storeCertList);
+                if ((CertSvc.ErrorCode)result != CertSvc.ErrorCode.CERTSVC_SUCCESS)
+                {
+                    Logger.Warn($"Unable to free certlist, result: {result}");
+                }
+                CertSvc.InstanceFree(instance);
+                return new List<certificateMetadata>();
+            }
+
+            return CertSvc.GetCertLists(instance, storeCertList, length);
+        }
+    }
+}
