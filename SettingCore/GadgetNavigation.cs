76558615643da59710587fb2ed19589aa90cbaf8
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2023-06-29 09:57:27 +0200

    update navigate-to method

--- SettingCore/GadgetNavigation.cs
+++ SettingCore/GadgetNavigation.cs
@@ -45,8 +45,6 @@ namespace SettingCore
                 }
                 NUIApplication.GetDefaultWindow().GetDefaultNavigator().EnableBackNavigation = true;
             };
-
-            NUIApplication.GetDefaultWindow().KeyEvent += GadgetNavigation_KeyEvent;
         }
 
         public static void SetFullScreenMode(bool fullScreen)
@@ -71,35 +69,49 @@ namespace SettingCore
 
         public static void NavigateTo(string menuPath)
         {
-            semaphore.Wait();
-
-            var info = GadgetManager.Instance.GetGadgetInfoFromPath(menuPath);
-            if (info == null)
-            {
-                Logger.Warn($"could not find gadget for menupath: {menuPath}");
-                semaphore.Release();
-                return;
-            }
-
-            if (gadgetPages.Values.Select(x => x.ClassName).Contains(info.ClassName))
-            {
-                Logger.Verbose($"Already navigated to menupath: {menuPath}");
-                semaphore.Release();
-                return;
-            }
-
             try
             {
-                var gadget = NUIGadgetManager.Add(info.Pkg.ResourceType, info.ClassName) as MenuGadget;
+                semaphore.Wait();
 
+                var info = GadgetManager.Instance.GetGadgetInfoFromPath(menuPath);
+                if (info == null)
+                {
+                    Logger.Warn($"could not find gadget for menupath: {menuPath}");
+                    return;
+                }
+
+                if (gadgetPages.Values.Select(x => x.ClassName).Contains(info.ClassName))
+                {
+                    Logger.Verbose($"Already navigated to menupath: {menuPath}");
+                    return;
+                }
+
+                var gadget = NUIGadgetManager.Add(info.Pkg.ResourceType, info.ClassName) as MenuGadget;
                 var title = gadget.ProvideTitle();
                 var content = gadget.MainView;
+
+                if (info.IsFullScreenMode)
+                {
+                    SetFullScreenMode(true);
+
+                    var contentPage = new ContentPage
+                    {
+                        CornerRadius = 0.SpToPx(),
+                        Content = content,
+                        ThemeChangeSensitive = true,
+                    };
+
+                    NUIApplication.GetDefaultWindow().GetDefaultNavigator().PushWithTransition(contentPage);
+                    gadgetPages.Add(contentPage, gadget);
+                    return;
+                }
 
                 // TODO: remove style customization with scalable unit, when merged to TizenFX
                 var appBarStyle = ThemeManager.GetStyle("Tizen.NUI.Components.AppBar") as AppBarStyle;
                 appBarStyle.TitleTextLabel.PixelSize = 24f.SpToPx();
 
-                var backButton = new Views.BackButton();            
+                var backButton = new BackButton();
+                backButton.Margin = new Extents(0, 8, 0, 0).SpToPx();
                 backButton.Clicked += (s, e) => NavigateBack();
 
                 var moreItems = new List<View>();
@@ -116,52 +128,34 @@ namespace SettingCore
                     moreItems.Add(moreButton);
                 }
 
-                if (info.IsFullScreenMode)
+                var page = new BaseContentPage
                 {
-                    SetFullScreenMode(true);
-
-                    var contentPage = new ContentPage
+                    // TODO: CornerRadius depends on SettingViewBorder.CornerRadius - SettingViewBorder.BorderLineThickness, which is defined at SettingView project.
+                    CornerRadius = (26.0f - 6.0f).SpToPx(),
+                    ClippingMode = ClippingModeType.ClipChildren,
+                    AppBar = new AppBar(appBarStyle)
                     {
-                        CornerRadius = 0.SpToPx(),
-                        Content = content,
+                        Size = new Size(-1, 64).SpToPx(),
+                        Title = title,
+                        NavigationContent = backButton,
                         ThemeChangeSensitive = true,
-                    };
+                        Actions = moreItems,
+                    },
+                    Content = content,
+                    ThemeChangeSensitive = true,
+                };
 
-                    NUIApplication.GetDefaultWindow().GetDefaultNavigator().PushWithTransition(contentPage);
-                    gadgetPages.Add(contentPage, gadget);
-                }
-                else
-                {
-                    backButton.Margin = new Extents(0, 8, 0, 0).SpToPx();
-
-                    var page = new BaseContentPage
-                    {
-                        // TODO: CornerRadius depends on SettingViewBorder.CornerRadius - SettingViewBorder.BorderLineThickness, which is defined at SettingView project.
-                        CornerRadius = (26.0f - 6.0f).SpToPx(),
-                        ClippingMode = ClippingModeType.ClipChildren,
-                        AppBar = new AppBar(appBarStyle)
-                        {
-                            Size = new Size(-1, 64).SpToPx(),
-                            Title = title,
-                            NavigationContent = backButton,
-                            ThemeChangeSensitive = true,
-                        },
-                        Content = content,
-                        ThemeChangeSensitive = true,
-                    };
-
-                    page.AppBar.Actions = moreItems;
-
-                    NUIApplication.GetDefaultWindow().GetDefaultNavigator().Push(page);
-                    gadgetPages.Add(page, gadget);
-                }
+                NUIApplication.GetDefaultWindow().GetDefaultNavigator().Push(page);
+                gadgetPages.Add(page, gadget);
             }
-            catch (System.Exception e) // TODO: add separate catch blocks for specific exceptions?
+            catch (Exception e)
             {
                 Logger.Error($"could not create gadget for menu path: {menuPath} => {e}");
             }
-
-            semaphore.Release();
+            finally
+            {
+                semaphore.Release();
+            }
         }
 
         private static Button GetMoreButton(IEnumerable<MoreMenuItem> moreMenu)
@@ -216,27 +210,6 @@ namespace SettingCore
             };
 
             return moreButton;
-        }
-
-        private static void GadgetNavigation_KeyEvent(object sender, Window.KeyEventArgs e)
-        {
-            if (NUIApplication.GetDefaultWindow().GetDefaultNavigator().ChildCount == 1)
-            {
-                return;
-            }
-
-            if (e.Key.State == Key.StateType.Down && e.Key.KeyPressedName == "XF86Back")
-            {
-                NUIApplication.GetDefaultWindow().GetDefaultNavigator().EnableBackNavigation = false;
-            }
-            else if (e.Key.State == Key.StateType.Up && e.Key.KeyPressedName == "XF86Back")
-            {
-                NavigateBack();
-            }
-            else
-            {
-                NUIApplication.GetDefaultWindow().GetDefaultNavigator().EnableBackNavigation = true;
-            }
         }
     }
 }
