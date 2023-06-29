ead7e80f1f47e14b6136429a91686f800271183b
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2023-06-29 09:55:52 +0200

    add setting navigator

---
+++ SettingCore/SettingNavigation.cs
@@ -0,0 +1,21 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.Components;
+
+namespace SettingCore
+{
+    public class SettingNavigation : Navigator
+    {
+        protected override void OnBackNavigation(BackNavigationEventArgs eventArgs)
+        {
+            if (PageCount > 1)
+            {
+                NUIApplication.GetDefaultWindow().GetDefaultNavigator().EnableBackNavigation = false;
+                GadgetNavigation.NavigateBack();
+            }
+            else
+            {
+                NUIApplication.Current?.Exit();
+            }
+        }
+    }
+}
