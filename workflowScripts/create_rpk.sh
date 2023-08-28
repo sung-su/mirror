688f2b9c5b8dd8aab9aa077a698b56dc2ea2404c
Piotr Czaja <p.czaja@samsung.com>
2023-08-28 11:50:37 +0200

    Fix creating rpk script.

--- workflowScripts/create_rpk.sh
+++ workflowScripts/create_rpk.sh
@@ -12,9 +12,9 @@ dotnet build SettingMainGadget/
 rm -rf SettingMainGadget/SettingMainGadget/create_rpk
 mkdir SettingMainGadget/SettingMainGadget/create_rpk
 cp -pr SettingMainGadget/SettingMainGadget/res/ SettingMainGadget/SettingMainGadget/create_rpk
-cp SettingMainGadget/SettingMainGadget/bin/Debug/netcoreapp3.1/SettingMainGadget.dll SettingMainGadget/SettingMainGadget/create_rpk/res/allowed/
-cp -pr SettingMainGadget/SettingMainGadget/bin/Debug/netcoreapp3.1/en-US/ SettingMainGadget/SettingMainGadget/create_rpk/res/allowed/
-cp -pr SettingMainGadget/SettingMainGadget/bin/Debug/netcoreapp3.1/ko-KR/ SettingMainGadget/SettingMainGadget/create_rpk/res/allowed/
+cp SettingMainGadget/SettingMainGadget/bin/Debug/net6.0/SettingMainGadget.dll SettingMainGadget/SettingMainGadget/create_rpk/res/allowed/
+cp -pr SettingMainGadget/SettingMainGadget/bin/Debug/net6.0/en-US/ SettingMainGadget/SettingMainGadget/create_rpk/res/allowed/
+cp -pr SettingMainGadget/SettingMainGadget/bin/Debug/net6.0/ko-KR/ SettingMainGadget/SettingMainGadget/create_rpk/res/allowed/
 cp SettingMainGadget/SettingMainGadget/tizen-manifest.xml SettingMainGadget/SettingMainGadget/create_rpk/
 
 /home/user/tizen-studio/tools/ide/bin/tizen package -t rpk -- SettingMainGadget/SettingMainGadget/create_rpk -s platform_profile
