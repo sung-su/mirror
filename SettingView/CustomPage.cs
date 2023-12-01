98110aa63108949ea5792c6a2ffef281bf83071b
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2023-12-01 17:26:41 +0100

    add custom page

---
+++ SettingView/CustomPage.cs
@@ -0,0 +1,25 @@
+﻿using Tizen.NUI;
+using Tizen.NUI.BaseComponents;
+using Tizen.NUI.Components;
+
+namespace SettingView
+{
+    public class CustomPage : Page
+    {
+        public TextLabel Title { get; set; }
+
+        public ScrollableBase Content { get; set;  }
+
+        public CustomPage()
+        {
+            WidthSpecification = LayoutParamPolicies.MatchParent;
+            HeightSpecification = LayoutParamPolicies.MatchParent;
+            Layout = new LinearLayout()
+            {
+                LinearOrientation = LinearLayout.Orientation.Vertical,
+            };
+            ThemeChangeSensitive = true;
+            CornerRadius = (SettingViewBorder.WindowCornerRadius - SettingViewBorder.WindowPadding).SpToPx();
+        }
+    }
+}
