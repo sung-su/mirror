57d94728faf2a3f2dffc2b3e7e3604560ba87903
Yurii Zinchuk/Tizen Services & IoT (PLT) /SRPOL/Engineer/Samsung Electronics <y.zinchuk@samsung.com>
2024-01-30 12:30:27 +0100

    add application item info

--- SettingMainGadget/SettingMainGadget/Apps/AppManager.cs
+++ SettingMainGadget/SettingMainGadget/Apps/AppManager.cs
@@ -1,7 +1,9 @@
 ﻿using SettingCore;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using Tizen.Applications;
 
 namespace SettingMainGadget.Apps
@@ -49,6 +51,47 @@ namespace SettingMainGadget.Apps
             }
 
             return listInfo.Where(a => a.Categories.Contains(category)).ToList();
+        }
+
+        public class ApplicationItemInfo : INotifyPropertyChanged
+        {
+            public event PropertyChangedEventHandler PropertyChanged;
+            public string AppId { get; }
+            public string PackageId { get; set; }
+            public string Name { get; set; }
+            public string IconPath { get; set; }
+            public long AppSize { get; set; }
+            public System.DateTime LastLaunchTime { get; set; }
+
+            private string size;
+            public string SizeToDisplay
+            {
+                get => size;
+                set
+                {
+                    if (value != size)
+                    {
+                        size = value;
+                        RaisePropertyChanged(nameof(SizeToDisplay));
+                    }
+                }
+            }
+
+            public ApplicationItemInfo(string appid, string name, string iconPath, string size)
+            {
+                AppId = appid;
+                Name = name;
+                IconPath = iconPath;
+                SizeToDisplay = size;
+            }
+
+            /// <summary>
+            /// Raises PropertyChanged event.
+            /// </summary>
+            protected void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
         }
     }
 }
