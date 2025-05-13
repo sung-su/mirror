40304368b18be5eb561c8faf3b13ba0afed390a8
Kangho Hur <kangho.hur@samsung.com>
2025-05-13 09:56:24 +0900

    Revert "Change source from db"
    This reverts commit 03279f17e064c25cf885010b5b8e1173c948b685.

--- lib/poc/media_list_poc.dart
+++ lib/poc/media_list_poc.dart
@@ -3,8 +3,6 @@ import 'package:provider/provider.dart';
 import 'package:tizen_fs/widgets/backdrop_scaffold.dart';
 import 'package:tizen_fs/widgets/immersive_list.dart';
 import 'package:tizen_fs/widgets/media_list.dart';
-import 'package:tizen_fs/models/category.dart';
-import 'package:tizen_fs/utils/media_db_parser.dart';
 
 class MediaListPocPage extends StatelessWidget {
   const MediaListPocPage({super.key});
@@ -27,7 +25,6 @@ class _HomeContentState extends State<HomeContent> {
   final ImmersiveListModel _immersiveListModel = ImmersiveListModel.fromMock();
   final ImmersiveAreaController _immersiveAreaController =
       ImmersiveAreaController();
-  late List<Category> _categories;
 
   @override
   void initState() {
@@ -35,10 +32,6 @@ class _HomeContentState extends State<HomeContent> {
     _immersiveListModel.addListener(() {
       setState(() {});
     });
-    _categories = Provider.of<MediaDBParser>(context, listen: false)
-        .categoryMap
-        .values
-        .toList();
   }
 
   @override
@@ -82,7 +75,6 @@ class _HomeContentState extends State<HomeContent> {
               },
             ),
             MediaList(
-              contents: _categories[0].tiles,
               title: 'Your apps',
               columns: ColumnCount.nine,
               onFocused: () {
@@ -97,7 +89,6 @@ class _HomeContentState extends State<HomeContent> {
               },
             ),
             MediaList(
-              contents: _categories[0].tiles,
               title: 'Top selling movies',
               columns: ColumnCount.four,
               onFocused: () {
@@ -112,7 +103,6 @@ class _HomeContentState extends State<HomeContent> {
               },
             ),
             MediaList(
-              contents: _categories[0].tiles,
               title: 'Popular shows',
               columns: ColumnCount.four,
               onFocused: () {
@@ -127,7 +117,6 @@ class _HomeContentState extends State<HomeContent> {
               },
             ),
             MediaList(
-              contents: _categories[0].tiles,
               title: 'Recomended videos',
               columns: ColumnCount.three,
               onFocused: () {
