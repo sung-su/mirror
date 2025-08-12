1218ce8eb761bd54bf00404cf918749ab0b37128
sung-su.kim <sung-su.kim@samsung.com>
2025-08-12 18:54:26 +0900

    Update first setting page

--- lib/settings/setting_page.dart
+++ lib/settings/setting_page.dart
@@ -1,10 +1,14 @@
-
 import 'package:flutter/material.dart';
 import 'package:tizen_fs/models/page_node.dart';
 import 'package:tizen_fs/widgets/setting_list_view.dart';
 
 class SettingPage extends StatefulWidget {
-  const SettingPage({super.key, required this.node, required this.isEnabled, required this.onSelectionChanged});
+  const SettingPage({
+    super.key,
+    required this.node,
+    required this.isEnabled,
+    required this.onSelectionChanged,
+  });
 
   final PageNode? node;
   final bool isEnabled;
@@ -14,7 +18,8 @@ class SettingPage extends StatefulWidget {
   State<SettingPage> createState() => _SettingPageState();
 }
 
-class _SettingPageState extends State<SettingPage> with AutomaticKeepAliveClientMixin {
+class _SettingPageState extends State<SettingPage>
+    with AutomaticKeepAliveClientMixin {
   GlobalKey<SettingListViewState> _listKey = GlobalKey<SettingListViewState>();
 
   @override
@@ -38,7 +43,7 @@ class _SettingPageState extends State<SettingPage> with AutomaticKeepAliveClient
   @override
   void didUpdateWidget(covariant SettingPage oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if(widget.isEnabled){
+    if (widget.isEnabled) {
       initFocus();
     }
   }
@@ -47,59 +52,50 @@ class _SettingPageState extends State<SettingPage> with AutomaticKeepAliveClient
   Widget build(BuildContext context) {
     super.build(context);
 
-    if (widget.node == null) {
-      return Container (
-        color: Theme.of(context).colorScheme.onTertiary
-      ) ;
-    }
-
-    if (widget.node!.builder != null) {
-      return Container(
-        color: widget.isEnabled ? Theme.of(context).colorScheme.surface : Theme.of(context).colorScheme.onTertiary,
-        child: widget.node!.builder?.call(context, widget.node!)
-      );
-    }
+    double titleHeight = 100;
+    double titleFontSize = 30;
 
     return Container(
-      color: widget.isEnabled ? Theme.of(context).colorScheme.surface : Theme.of(context).colorScheme.onTertiary,
+      color:
+          widget.isEnabled
+              ? Theme.of(context).colorScheme.surface
+              : Theme.of(context).colorScheme.onTertiary,
       child: Column(
         children: [
-          SizedBox(
-            height: 120,
-            child: Padding(
-              padding: widget.isEnabled ? const EdgeInsets.fromLTRB(120, 0, 130, 0) : const EdgeInsets.fromLTRB(80, 0, 80, 0),
+          Padding(
+            padding: widget.isEnabled ? EdgeInsets.fromLTRB(80, 35, 0, 10) : EdgeInsets.fromLTRB(60, 35, 0, 10),
+            child: SizedBox(
+              height: titleHeight,
               child: Align(
-                alignment: Alignment.bottomLeft,
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+                  alignment: Alignment.bottomLeft,
+                  child: Container(
                     child: Text(
-                      widget.node?.title ?? '',
-                      style: TextStyle(
-                        fontSize: 32
-                      ),
+                        widget.node?.title ?? '',
+                        style: TextStyle(fontSize: titleFontSize),
                     ),
-                  )
                 ),
               ),
-            )
+            ),
           ),
           if (!widget.node!.children.isEmpty)
-          Expanded(
-            child: Align(
-              alignment: Alignment.topLeft,
-              child: Padding(
-                padding: widget.isEnabled ? const EdgeInsets.symmetric(horizontal: 85) : const EdgeInsets.symmetric(horizontal: 50),
-                child: SettingListView(
-                  key: _listKey,
-                  node: widget.node!,
-                  onSelectionChanged: (selected) {
-                    widget.onSelectionChanged?.call(selected);
-                  },
+            Expanded(
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Padding(
+                  padding:
+                      widget.isEnabled
+                          ? const EdgeInsets.symmetric(horizontal: 35)
+                          : const EdgeInsets.symmetric(horizontal: 20),
+                  child: SettingListView(
+                    key: _listKey,
+                    node: widget.node!,
+                    onSelectionChanged: (selected) {
+                      widget.onSelectionChanged?.call(selected);
+                    },
+                  ),
                 ),
-            )
-                      ),
-          )
+              ),
+            ),
         ],
       ),
     );
