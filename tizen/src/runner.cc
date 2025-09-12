361c961b5297b4340cc4ebc25b8e2d7cedeb97a3
Kangho Hur <kangho.hur@samsung.com>
2025-09-12 11:30:43 +0900

    Update user_pixel_ration correctly

--- tizen/src/runner.cc
+++ tizen/src/runner.cc
@@ -1,11 +1,61 @@
-#include "runner.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <dlfcn.h>
+#include <dlog.h>
 
+#include "runner.h"
 #include "generated_plugin_registrant.h"
+
+// 함수 포인터 타입 정의
+typedef void* (*ecore_wl2_display_connect_t)(const char *name);
+typedef int (*ecore_wl2_init_t)(void);
+typedef void (*ecore_wl2_sync_t)(void);
+typedef void* (*ecore_wl2_display_get_t)(void *display);
+typedef void (*ecore_wl2_display_screen_size_get_t)(void *display, int *w, int *h);
 
 class App : public FlutterApp {
  public:
+  static inline const char *tag = "ConsoleMessage";
+
   bool OnCreate() {
-    user_pixel_ratio_ = 2.0;
+    void *handle = dlopen("libecore_wl2.so.1", RTLD_LAZY);
+    if (!handle) {
+        fprintf(stderr, "Cannot open library: %s\n", dlerror());
+    }
+
+    ecore_wl2_display_connect_t ecore_wl2_display_connect = (ecore_wl2_display_connect_t) dlsym(handle, "ecore_wl2_display_connect");
+    ecore_wl2_init_t ecore_wl2_init = (ecore_wl2_init_t) dlsym(handle, "ecore_wl2_init");
+    ecore_wl2_sync_t ecore_wl2_sync = (ecore_wl2_sync_t) dlsym(handle, "ecore_wl2_sync");
+    ecore_wl2_display_get_t ecore_wl2_display_get = (ecore_wl2_display_get_t) dlsym(handle, "ecore_wl2_display_get");
+    ecore_wl2_display_screen_size_get_t ecore_wl2_display_screen_size_get = (ecore_wl2_display_screen_size_get_t) dlsym(handle, "ecore_wl2_display_screen_size_get");
+
+    char *error;
+    if ((error = dlerror()) != NULL) {
+        dlog_print(DLOG_ERROR, tag, "Cannot load symbol: %s\n", error);
+        dlclose(handle);
+    }
+
+    if (ecore_wl2_init() != 0) {
+        dlog_print(DLOG_ERROR, tag, "Failed to initialize ecore_wl2\n");
+        dlclose(handle);
+    }
+
+    void *display = ecore_wl2_display_connect(NULL);
+    if (display == NULL) {
+        dlog_print(DLOG_ERROR, tag, "Failed to connect to display\n");
+        dlclose(handle);
+    }
+    else {
+      ecore_wl2_sync();
+
+      int width, height;
+      ecore_wl2_display_screen_size_get(display, &width, &height);
+      dlog_print(DLOG_INFO, tag, "Screen size: %dx%d\n", width, height);
+      dlclose(handle);
+
+      user_pixel_ratio_ = width / 960;
+    }
+
     if (FlutterApp::OnCreate()) {
       RegisterPlugins(this);
     }
